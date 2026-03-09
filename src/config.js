4e42ee0a8a871793b9618c2793a9a0fd6e5a61d1
Taikiy49 <taikiy49@gmail.com>
2026-03-09 12:03:28 -1000

    tcp changes

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:7000";
 export default API_URL;
