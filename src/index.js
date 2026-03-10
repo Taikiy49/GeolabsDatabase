cd0651eb570298ea62a64bd67f2812ddf0e278ec
Taikiy49 <taikiy49@gmail.com>
2026-03-10 10:23:16 -1000

    good

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
-
 import App from "./App";
 import { msalConfig } from "./authConfig";
 
